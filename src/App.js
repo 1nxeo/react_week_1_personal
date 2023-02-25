bd8b7cc54eaee90a0fc2d3cc5e549b878ab7b662
Inseo Yang <y.1nseooo@gmail.com>
2023-02-25 20:36:41 +0900

    components divide

--- src/App.js
+++ src/App.js
@@ -1,31 +1,8 @@
 import React, {useState} from 'react';
 import "./App.css"
+import Done from './components/Done';
+import Working from './components/Working';
 
-function Working(props){
-  return (
-    <div className='todo-box'>
-      <div className='content-box'>
-        <div className='title'>{props.todolist.title}</div>
-        <div className='desc'>{props.todolist.desc}</div><br/>
-        <button className="btns" onClick={() => props.handleRemove(props.todolist.id)}>삭제</button>
-        <button className="btns" onClick={() => props.handleChangeDone(props.todolist.id)}>완료</button>
-      </div>
-    </div>
-  )
-}
-
-function Done(props){
-  return (
-    <div className='todo-box'>
-      <div className='content-box'>
-        <div className='title'>{props.todolist.title}</div>
-        <div className='desc'>{props.todolist.desc}</div><br/>
-        <button className="btns" onClick={() => props.handleRemove(props.todolist.id)}>삭제</button>
-        <button className="btns" onClick={() => props.handleChangeDone(props.todolist.id)}>취소</button>
-      </div>
-    </div>
-  )
-}
 
 function App() {
 
---
+++ src/components/Working.jsx
@@ -0,0 +1,14 @@
+function Working(props){
+    return (
+    <div className='todo-box'>
+        <div className='content-box'>
+        <div className='title'>{props.todolist.title}</div>
+        <div className='desc'>{props.todolist.desc}</div><br/>
+        <button className="btns" onClick={() => props.handleRemove(props.todolist.id)}>삭제</button>
+        <button className="btns" onClick={() => props.handleChangeDone(props.todolist.id)}>완료</button>
+        </div>
+    </div>
+    )
+}
+
+export default Working
